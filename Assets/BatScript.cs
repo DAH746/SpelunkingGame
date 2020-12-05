7712f3443f16786ce6f233b0821f3051a1847705
ec18001 <d.hussain@se18.qmul.ac.uk>
2020-12-05 17:11:24 +0000

    1/4 level 2 complete

--- Assets/BatScript.cs
+++ Assets/BatScript.cs
@@ -44,6 +44,7 @@ public class BatScript : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         Physics2D.IgnoreLayerCollision(11, 0, true); //ignore collision between "Enemy layer - layer 11" and the "default layer - layer 0 "
         Physics2D.IgnoreLayerCollision(11, 12, false);
+        Physics2D.IgnoreLayerCollision(11, 31, true);
     }
 
     void Update()
