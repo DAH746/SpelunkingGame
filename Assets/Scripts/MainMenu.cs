7de97892e53275cd290b1d0148125567e5e7b9d2
Ryan <ryan@ryanmaber.com>
2020-12-05 18:49:24 +0000

    Added audio volume slider and options menu to start screen.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu = null;
+    public GameObject optionsScreen = null;
+
+    public AudioSource backgroundMusic = null;
+
+    float gameVolume = 0.5f;
+
     public void startGame()
     {
+        PlayerPrefs.SetFloat("volume", gameVolume);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        optionsScreen.SetActive(true);
+    }
+
+    public void main()
+    {
+        optionsScreen.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void setGameVolume(float volume)
+    {
+        gameVolume = volume;
+        backgroundMusic.volume = volume;
     }
 }
